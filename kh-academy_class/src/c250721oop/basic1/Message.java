d885a49d8a7c03b3e24a0b6e7155f18cec27a5da
NDDYLYS <nodvic@naver.com>
2025-07-21 15:40:49 +0900

    dsf

---
+++ kh-academy_class/src/c250721oop/basic1/Message.java
@@ -0,0 +1,12 @@
+package c250721oop.basic1;;
+
+// 프로그렘에게 알려줄 신규 데이터 클래스
+// main을 만들면 안됨
+// 구성요소를 내부에 변수로 선언하며 값은 주지 않는다
+public class Message 
+{
+	String sender;
+	String content;
+	String time;
+	int remain;
+}

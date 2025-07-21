7187da51dc31ad257aa6cfe47d9237dbebe6e311
NDDYLYS <nodvic@naver.com>
2025-07-21 16:02:25 +0900

    a

---
+++ kh-academy_class/src/c250721oop/basic2/Country.java
@@ -0,0 +1,15 @@
+package c250721oop.basic2;
+
+public class Country 
+{
+	String name;
+	String capital;
+	long population;
+	
+	public Country(String _name, String _capital, long _population) 
+	{
+		name = _name;
+		capital = _capital;
+		population = _population;
+	}
+}

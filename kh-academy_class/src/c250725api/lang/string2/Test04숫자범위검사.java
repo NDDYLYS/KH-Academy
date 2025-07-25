7f35b7b4fff6733dd08333d01d59ffadd98f4a0b
NDDYLYS <nodvic@naver.com>
2025-07-25 18:42:08 +0900

    asd

--- kh-academy_class/src/c250725api/lang/string2/Test04숫자범위검사.java
+++ kh-academy_class/src/c250725api/lang/string2/Test04숫자범위검사.java
@@ -12,7 +12,14 @@ public class Test04숫자범위검사
     	Scanner sc = new Scanner(System.in);
     	System.out.print("검사할 정수를 입력해주세요. : ");
     	String input = sc.next();
+    	System.out.println(input);
     	
+    	String temp = input.substring(0, 4);
+    	int year = Integer.parseInt(temp);
+    	
+    	String temp2 = input.substring(5, 7);
+    	int month = Integer.parseInt(temp2);
+    	// System.out.println(temp2);
     	// YYYY-MM-DD 형식 정규식 작성
     	// 월 상관없이 무조건 31까지
     	// 월을 구분하여 28, 30, 31로 구분
@@ -20,8 +27,38 @@ public class Test04숫자범위검사
     	// String.substring()
     	// Integer.parseInt()
     	
-    	// XXXX-(0[1-9]|1[0-2])-XX
-    	String regex = "";
+    	boolean leapYear = false;
+		
+		boolean leapYear1 = (year % 4 == 0);		
+		boolean leapYear2 = (year % 100 == 0);
+		boolean leapYear3 = (year % 400 == 0);
+		
+		leapYear = (leapYear3 || (!leapYear2 && leapYear1));
+		String regex = "";
+
+		switch(month) 
+		{
+		case 1 : 
+		case 3 : 
+		case 5 : 
+		case 7 : 
+		case 8 : 
+		case 10 : 
+			regex = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1])$";
+			break;
+		case 4 : 
+		case 6 : 
+		case 9 : 
+		case 11 : 
+			regex = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|30)$";
+			break;
+		case 2 : 
+			if (!leapYear)
+				regex = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-8])$";
+			else 
+				regex = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9])$";
+			break;
+		}
     
     	boolean match = input.matches(regex);
     	sc.close();

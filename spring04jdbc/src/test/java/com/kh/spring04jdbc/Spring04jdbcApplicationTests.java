254b7085994aa4b9629c062f022bcf4864bab106
NDDYLYS <nodvic@naver.com>
2025-08-12 16:08:35 +0900

    add oracle connector

---
+++ spring04jdbc/src/test/java/com/kh/spring04jdbc/Spring04jdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.spring04jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring04jdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

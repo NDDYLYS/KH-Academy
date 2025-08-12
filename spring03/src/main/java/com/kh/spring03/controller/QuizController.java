cf25d7d58df9b7c8199518059e2b17dc77ec4235
NDDYLYS <nodvic@naver.com>
2025-08-12 15:13:05 +0900

    add spring

---
+++ spring03/src/main/java/com/kh/spring03/controller/QuizController.java
@@ -0,0 +1,71 @@
+package com.kh.spring03.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuizController 
+{
+	@RequestMapping("/quiz01")
+	public String Quiz01(int cream, int nutella) 
+	{
+		return "크림와플 : " + cream * 2500 + "원, "
+				+ "누텔라와플 : " + nutella * 3500 + "원";
+	}
+	
+	@RequestMapping("/quiz02")
+	public String Quiz02(int year) 
+	{
+		int now = 2025;
+		int koreanAge = now - year + 1;
+		
+		String subwayCategory = "";
+		String subwayPrice;
+		
+		if (20 <= koreanAge && koreanAge <= 64) 
+		{
+			subwayCategory = "성인(20~64)";
+			subwayPrice = "1550원";
+		}
+		else if (14 <= koreanAge && koreanAge <= 19) 
+		{
+			subwayCategory = "청소년(14~19)";
+			subwayPrice = "900원";
+		}
+		else if (8 <= koreanAge && koreanAge <= 13) 
+		{
+			subwayCategory = "어린이(8~13)";
+			subwayPrice = "550원";
+		}
+		else
+		{
+			subwayCategory = "어르신 및 영유아(65~ || ~7)";
+			subwayPrice = "무료";
+		}
+		
+		return year + "년 출생한 사람은 " + subwayCategory + "이며 지하철 요금은 " + subwayPrice + "입니다.";
+	
+	}
+	
+	@RequestMapping("/quiz03")
+	public String Quiz03(int height, int weight) 
+	{
+		float tall2 = height / 100f;
+		float bmi = weight / (float)Math.pow(tall2, 2);
+		
+		return "당신의 BMI 지수는 " + bmi + "입니다.";
+	}
+	
+	@RequestMapping("/quiz04")
+	public String Quiz04(int year) 
+	{
+		boolean leapYear = false;
+		
+		boolean leapYear1 = (year % 4 == 0);		
+		boolean leapYear2 = (year % 100 == 0);
+		boolean leapYear3 = (year % 400 == 0);
+		
+		leapYear = (leapYear3 || (!leapYear2 && leapYear1));
+		return (leapYear)? "윤년입니다." : "평년입니다.";
+	}
+}
--- spring03/src/main/java/com/kh/spring03/controller/TestController.java
+++ spring03/src/main/java/com/kh/spring03/controller/TestController.java
@@ -1,6 +1,11 @@
 package com.kh.spring03.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +26,7 @@ public class TestController
 	// @RequestMapping 키워드를 메소드 상단에 추가
 	// 메소드는 사용자가 받을 내용을 내보내면 된다
 	// 주소는 http://localhost:8080 뒤에 올 값을 작성
+	// 등록을 해야 사용이 된다
 	@RequestMapping("/hello")
 	public String hello() 
 	{
@@ -40,5 +46,64 @@ public class TestController
 		Random r = new Random();
 		int dice = r.nextInt(6) + 1;
 		return "DICE" + dice;
+	}
+	
+	@RequestMapping("/test02")
+	public String lotto1() 
+	{
+		Random r = new Random();
+		int lotto = r.nextInt(45) + 1;
+		return "LOTTO1 : " + lotto;
+	}
+	
+	@RequestMapping("/test03")
+	public String lotto2() 
+	{
+		List<Integer> LottoList = new ArrayList<>();
+		for(int i = 0; i < 45; i++)
+		{
+			LottoList.add(i + 1);
+		}
+		
+		Collections.shuffle(LottoList);
+		String text = "";
+		for(int i = 0; i < 6; i++)
+		{
+			text += LottoList.get(i) + ", ";
+		}
+		
+		return "LOTTO2 : " + text;
+	}
+	
+//	@RequestMapping("/test04")
+//	public Set<Integer> lotto3() 
+//	{
+//		Set<Integer> lottos = new TreeSet<>();
+//		Random r = new Random();
+//		
+//		while(lottos.size() < 6) 
+//		{
+//			int num = r.nextInt(45) + 1;
+//			lottos.add(num);
+//		}
+//		
+//		return lottos;
+//	}
+	
+	// 주소에 포함된 파라미터를 읽는 방법
+	// 주소에 있는 ?가 포함된 뒷부분을 파라미터로 간주
+	// 파라미터는 ?a=1&b=2&c=3 형태, 개수가 다르다
+	// 컨트롤러에서 이름과 형태를 맞춰서 파라미터를 읽을 수 있다
+	
+	@RequestMapping("/test04")
+	public String getstring(String query) 
+	{
+		return query + "에 대한 검색 결과";
+	}
+	
+	@RequestMapping("/test05")
+	public Double query(int usd) 
+	{
+		return usd * 1389.03;
 	}
 }

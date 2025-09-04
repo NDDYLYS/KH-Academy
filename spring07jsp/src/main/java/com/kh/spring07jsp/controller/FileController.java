f2e6b41ae068d20dd9188c87c61cba3ddcaaa577
NDDYLYS <nodvic@naver.com>
2025-09-04 17:40:01 +0900

    add

--- spring07jsp/src/main/java/com/kh/spring07jsp/controller/FileController.java
+++ spring07jsp/src/main/java/com/kh/spring07jsp/controller/FileController.java
@@ -3,6 +3,7 @@ package com.kh.spring07jsp.controller;
 import java.io.File;
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,10 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.kh.spring07jsp.dao.AttachmentDao;
+import com.kh.spring07jsp.dto.AttachmentDto;
+
 @Controller
 @RequestMapping("/file")
 public class FileController 
 {
+	@Autowired
+	private AttachmentDao attachmentDao;
+	
 	@RequestMapping("/test01")
 	public String test01() {
 		return "/WEB-INF/views/file/test01.jsp";
@@ -40,6 +47,8 @@ public class FileController
 	@PostMapping("/test04")
 	public String test04 (@RequestParam String uploader, @RequestParam MultipartFile attach) throws IllegalStateException, IOException 
 	{
+		int attachmentNo = attachmentDao.sequence();
+		
 		System.out.println("uploader : " + uploader); // 업로드한 입력창 이름
 		System.out.println("업로드한 입력창 이름 : " + attach.getName()); // 업로드한 입력창 이름
 		System.out.println("파일명 : " + attach.getOriginalFilename()); // 파일명
@@ -47,15 +56,27 @@ public class FileController
 		System.out.println("업로드한 파일 유형 : " + attach.getContentType()); // 업로드한 파일 유형
 		System.out.println("고유번호 : " + attach); // 고유번호
 		
-		File home = new File(System.getProperty("user.home"));
-		File upload = new File(home, "upload");
-		if (upload.exists() == false) 
+		if (!attach.isEmpty()) 
 		{
-			upload.mkdirs();
+			File home = new File(System.getProperty("user.home"));
+			File upload = new File(home, "upload");
+			if (upload.exists() == false) 
+			{
+				upload.mkdirs();
+			}
+			
+			File target = new File(upload, String.valueOf(attachmentNo)); // 저장할 파일의 인스턴스
+			attach.transferTo(target);
 		}
 		
-		File target = new File(upload, attach.getOriginalFilename()); // 저장할 파일의 인스턴스
-		attach.transferTo(target);
+
+		AttachmentDto attachmentDto = new AttachmentDto();
+		attachmentDto.setAttachmentNo(attachmentNo);
+		attachmentDto.setAttachmentName(attach.getOriginalFilename());
+		attachmentDto.setAttachmentType(attach.getContentType());
+		attachmentDto.setAttachmentSize(attach.getSize());
+		
+		attachmentDao.insert(attachmentDto);
 		
 		return "redirect:test01";
 	}

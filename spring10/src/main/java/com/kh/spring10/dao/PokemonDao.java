47fcc962a70aadeb5d0fd7a8304fbd327a300037
sooyeon8888 <pansy0828sy@gmail.com>
2025-11-13 14:46:26 +0900

    student -> mybatis confirm

--- spring10/src/main/java/com/kh/spring10/dao/PokemonDao.java
+++ spring10/src/main/java/com/kh/spring10/dao/PokemonDao.java
@@ -1,12 +1,9 @@
 package com.kh.spring10.dao;
 
-import java.util.List;
-import java.util.Set;
-
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
 import com.kh.spring10.dto.PokemonDto;
 
 @Repository

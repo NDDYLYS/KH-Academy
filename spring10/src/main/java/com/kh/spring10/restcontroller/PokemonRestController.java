1a63987905c1ead70ac443814a99476f8effc78d
NDDYLYS <nodvic89@gmail.com>
2025-11-14 15:49:27 +0900

    add edit temp

--- spring10/src/main/java/com/kh/spring10/restcontroller/PokemonRestController.java
+++ spring10/src/main/java/com/kh/spring10/restcontroller/PokemonRestController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -68,4 +70,16 @@ public class PokemonRestController
 			throw new TargetNotfoundException("존재하지 않는 포켓몬");
 		pokemonDao.delete(pokemonNo);
 	}
+	
+//	@PutMapping("/[pokemonNo]")
+//	public void edit(@PathVariable int pokemonNo) 
+//	{
+//		
+//	}
+//	
+//	@PatchMapping("/[pokemonNo]")
+//	public void edit(@PathVariable int pokemonNo) 
+//	{
+//		
+//	}
 }

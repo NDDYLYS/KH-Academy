b071e54faee7ee07995088fd9da7d1336222b466
NDDYLYS <nodvic89@gmail.com>
2025-11-26 17:48:24 +0900

    SpringDoc에다가 로그인 후 토큰 받는 기능 추가

--- spring10/src/main/java/com/kh/spring10/configurtion/SpringDocConfiguration.java
+++ spring10/src/main/java/com/kh/spring10/configurtion/SpringDocConfiguration.java
@@ -3,8 +3,12 @@ package com.kh.spring10.configurtion;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SpringDocConfiguration {
@@ -16,6 +20,17 @@ public class SpringDocConfiguration {
 		info.setTitle("KH 정보교육원 수업용 REST API");
 		info.setDescription("ReactJS와 통신");
 		
-		return new OpenAPI().info(info);
+		// 추가 JWT 인증방식 적용, 이를 반영
+		String jwtHeaderName = "Authorization";
+		SecurityRequirement requirement = new SecurityRequirement();
+		requirement.addList(jwtHeaderName);
+		
+		Components components = new Components();
+		components.addSecuritySchemes(jwtHeaderName, 
+				new SecurityScheme().name(jwtHeaderName)
+				.type(SecurityScheme.Type.HTTP).scheme("bearer"));
+		
+		
+		return new OpenAPI().info(info).addSecurityItem(requirement).components(components);
 	}
 }

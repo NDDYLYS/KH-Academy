6a07424f9836730aa57f13ee4f69a122cd37fb93
NDDYLYS <nodvic89@gmail.com>
2025-12-02 08:59:46 +0900

    웹소켓 붙이고 서버 구동 성공

--- spring10/src/main/java/com/kh/spring10/configurtion/WebSocketConfiguration.java
+++ spring10/src/main/java/com/kh/spring10/configurtion/WebSocketConfiguration.java
@@ -1,11 +1,24 @@
 package com.kh.spring10.configurtion;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @EnableWebSocketMessageBroker
 @Configuration
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer{
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");     // 구독 prefix
+        registry.setApplicationDestinationPrefixes("/pub"); // 발행 prefix
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*");
+    }
+	
 }

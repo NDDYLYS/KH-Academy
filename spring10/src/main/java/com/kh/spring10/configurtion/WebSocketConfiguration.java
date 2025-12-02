496739deb7aefe1a8cb2dfadace6625e189a5546
NDDYLYS <nodvic89@gmail.com>
2025-12-02 09:40:10 +0900

    웹소켓 설정 완료

--- spring10/src/main/java/com/kh/spring10/configurtion/WebSocketConfiguration.java
+++ spring10/src/main/java/com/kh/spring10/configurtion/WebSocketConfiguration.java
@@ -10,15 +10,20 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer{
 
+	//////////////////////////////
+	// 웹소켓 붙이고도 서버 구동 성공을 위해 추가한 코드
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");     // 구독 prefix
-        registry.setApplicationDestinationPrefixes("/pub"); // 발행 prefix
+    	registry.setApplicationDestinationPrefixes("/app"); // 발행 prefix
+        registry.enableSimpleBroker("/public", "/private");     // 구독 prefix 공개/비공개
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*");
+        registry
+        .addEndpoint("/ws")
+        .setAllowedOriginPatterns("*") // 접속 가능한 패턴 설정
+        .withSockJS(); // 웹소켓 성능 향상(웹소켓을 HTTP처럼 쓰게 해줌)
     }
-	
+	//////////////////////////////
 }
